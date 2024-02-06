d45a9e7e08109bcc04516cf12e59a70738476706
Maxime <malryck33@gmail.com>
2024-02-06 12:29:51 +0100

    Initialization

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section id="sideBar">
+        <h1>Domenc Maxime</h1>
+        <h2>Développeur Fullstack</h2>
+        <span></span>
+      </section>
+
+      <section id="aboutMe">
+
+      </section>
+
+      <section id="projects">
+
+      </section>
+
+      <section id="closing">
+        
+      </section>
     </div>
   );
 }
